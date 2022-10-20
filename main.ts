6f060e33cd18de54a71382ff41b4be119646b5c3
cuigods <115679949+cuigods@users.noreply.github.com>
2022-10-20 11:03:03 +0800

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,54 @@
-basic.forever(function () {
-	
+let count = 0
+input.onButtonPressed(Button.A, function () {
+    count = -1
+})
+input.onButtonPressed(Button.B, function () {
+    count = 9
+    basic.showNumber(count)
+    while (count > 0) {
+        count += -1
+        basic.showNumber(count)
+    }
+    basic.showLeds(`
+        . . . . .
+        . . . . .
+        . . # . .
+        . . . . .
+        . . . . .
+        `)
+    basic.showLeds(`
+        . . . . .
+        . . # . .
+        . # . # .
+        . . # . .
+        . . . . .
+        `)
+    basic.showLeds(`
+        . . # . .
+        . # . # .
+        # . . . #
+        . # . # .
+        . . # . .
+        `)
+    basic.showLeds(`
+        . # . # .
+        # . . . #
+        . . . . .
+        # . . . #
+        . # . # .
+        `)
+    basic.showLeds(`
+        # . . . #
+        . . . . .
+        . . . . .
+        . . . . .
+        # . . . #
+        `)
+    basic.showLeds(`
+        . . . . .
+        . . . . .
+        . . . . .
+        . . . . .
+        . . . . .
+        `)
 })
